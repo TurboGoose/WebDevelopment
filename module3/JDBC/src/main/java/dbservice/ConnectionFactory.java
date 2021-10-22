a4fe4926e9bf189199bfcbc57a8fe5be96040783
TurboGoose <antonpuckich@gmail.com>
2021-10-22 18:03:35 +0300

    Exclude h2 from dependencies

--- module3/JDBC/src/main/java/dbservice/ConnectionFactory.java
+++ module3/JDBC/src/main/java/dbservice/ConnectionFactory.java
@@ -1,18 +1,15 @@
 package dbservice;
 
-import org.h2.jdbcx.JdbcDataSource;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public enum ConnectionType {POSTGRES, H2}
+    public enum ConnectionType {POSTGRES}
 
     public static Connection get(ConnectionType type) {
         return switch (type) {
             case POSTGRES -> getPostgresConnection();
-            case H2 -> getH2Connection();
         };
     }
 
@@ -21,24 +18,6 @@ public class ConnectionFactory {
             return DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres?user=postgres&password=password");
         } catch (SQLException exc) {
             exc.printStackTrace();
-        }
-        return null;
-    }
-
-    private static Connection getH2Connection() {
-        try {
-            String url = "jdbc:h2:./h2db";
-            String name = "tully";
-            String pass = "tully";
-
-            JdbcDataSource ds = new JdbcDataSource();
-            ds.setURL(url);
-            ds.setUser(name);
-            ds.setPassword(pass);
-
-            return DriverManager.getConnection(url, name, pass);
-        } catch (SQLException e) {
-            e.printStackTrace();
         }
         return null;
     }
